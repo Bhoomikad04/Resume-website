3802ba3e1100f4fccc17b1089fa153aa41d6c2ab
Bhoomikad04 <bhoomikadoddagatti1503@gmail.com>
2024-12-01 10:17:25 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
-$(document).ready(function () {
-    $(".skills-title").click(function () {
-        $(".skills-list").toggle();
-    });
-});
+function home() {
+    window.location.href = 'index.html';
+}
+function resume() {
+    window.location.href = 'resume.html';
+}
+
+function biodata() {
+    window.location.href = 'biodata.html';
+}
